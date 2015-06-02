7497f1ab722ab97f0848d68ff9602249d6724502
Shamir Alavi <alavi1223@hotmail.com>
2015-06-02 17:46:57 -0400

    Data Cleaner class (final)

--- testCSV.py
+++ testCSV.py
@@ -1,4 +1,7 @@
 import pandas
 
+class DataCleaner
+    
+
 csvfile = 'C:\\Users\\shamiralavi\\Documents\\Data_Multisensor\\1. Jab\\new position\\run 2 (final)\\sensor15_jab.csv'
 file1 = pandas.read_csv(csvfile)
