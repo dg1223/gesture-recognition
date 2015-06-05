15f63fd1b8b125d9e03f6c180c1eb8b8aff06aae
Shamir Alavi <alavi1223@hotmail.com>
2015-06-04 20:59:13 -0400

    working version

--- whatever.py
+++ whatever.py
@@ -2,22 +2,40 @@ import pandas
 import numpy as np
 import os
 
-gesture_path = 'C:\\Users\\Shamir\\Desktop\\Grad\\Gesture Stuff\\Data_Multisensor\\'
-gesture_list = os.listdir((gesture_path))               # '1. Jab\\2. Uppercut\\3.Throw\\4. Jets\\5. Block\\6. Asgard\\'
-dataset_list = os.listdir((gesture_list))
-files_list =                       # datasets
+gesture_path = 'C:\\Users\\Shamir\\Desktop\\Grad\\Gesture Stuff\\Data_Multisensor\\'    # use input() to make it interactive
+destination =  'C:\\Users\\Shamir\\Desktop\\broken down files\\'
+fileformat = '.csv'
+backslash = '\\'
 
-count = 0
-for i in range(len(os.listdir(gesture_path))):        # we have 6 files corresponding to 6 gestures
-    gesture = os.listdir(gesture_path)[i]
-    for j in range(len(dataset_list)):                  # we have 3 files corresponding to 3 datasets (train, cross-validation, test)
-        dataset = os.listgesture_path + gesture + os.listdir()
-        for k in range(len(files_list)):                # we have 5 sensors (15,16,17,18,19) 
-            csvfile = gesture_path + gesture + dataset + files_list
+count = 1
+for i in range(len(os.listdir(gesture_path))):                                          # we have 6 files corresponding to 6 gestures
+    gesture = os.listdir(gesture_path)[i]                                               # Jab, Uppercut, Throw, Jets, Block, Asgard
+    
+    for j in range(len(os.listdir(gesture_path + gesture))):                            # we have 3 files corresponding to 3 datasets (train, cross-validation, test)
+        dataset = os.listdir(gesture_path + gesture)[j]                                 # Train, Cross Validation, Test
+        
+        for k in range(len(os.listdir(gesture_path + gesture + backslash + dataset))):  # we have 5 sensors (15,16,17,18,19) 
+            file = os.listdir(gesture_path + gesture + backslash + dataset)[k]          # desired csv file in the folder           
+            csvfile = gesture_path + gesture + backslash + dataset + backslash + file   # full filepath
+            print csvfile
+            readFile = pandas.read_csv(csvfile, header = None)                          # read csv file
+            
+            qr = readFile.loc[:, range(0, readFile.shape[1], 5)]                            # qr columns only
+            qr.to_csv(destination + str(count) + fileformat, header = False, index = False)
+            count += 1
+            
+            qx = readFile.loc[:, range(1, readFile.shape[1], 5)]                            # qx columns only
+            qx.to_csv(destination + str(count) + fileformat, header = False, index = False)
+            count += 1
+            
+            qy = readFile.loc[:, range(2, readFile.shape[1], 5)]                            # qy columns only  
+            qy.to_csv(destination + str(count) + fileformat, header = False, index = False)
+            count += 1
+            
+            qz = readFile.loc[:, range(3, readFile.shape[1], 5)]                            # qz columns only
+            qz.to_csv(destination + str(count) + fileformat, header = False, index = False)
+            count += 1
+            
 
-csvfile = 'C:\\Users\\Shamir\\Desktop\\Grad\\Gesture Stuff\\Data_Multisensor\\1. Jab\\1. Train\\sensor15_jab.csv'
-file1 = pandas.read_csv(csvfile, header = None)
-
-array = file1.loc[:,range(0,file1.shape[1],5)]
     
     
