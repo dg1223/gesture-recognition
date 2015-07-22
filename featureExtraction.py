95bf07ba1ef234f3400d4831b82c212ea14f7c1c
Shamir Alavi <alavi1223@hotmail.com>
2015-07-21 21:45:52 -0400

    Feature Extraction
    Added Cartesian Velocity

--- featureExtraction.py
+++ featureExtraction.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 from natsort import natsorted
 from pandas import DataFrame
+from scipy.spatial.distance import euclidean
 
 source_left = 'C:\\Users\\Shamir\\Desktop\\Hands_Sorted\\Left\\'                       # source folder
 source_right = 'C:\\Users\\Shamir\\Desktop\\Hands_Sorted\\Right\\'
@@ -21,8 +22,10 @@ destination_left  =  'C:\\Users\\Shamir\\Desktop\\Features\\Left\\'             
 destination_right =  'C:\\Users\\Shamir\\Desktop\\Features\\Right\\'            # gestures performed only with the right hand go here
 fileformat = '.csv'
 count = 1
+frequency = 110                                                                 # frequency = 110 Hz
 
 
+# function for extracting Variance
 def Variance(number_of_rows, sourceFile):
     
     variance = ['variance']
@@ -33,6 +36,7 @@ def Variance(number_of_rows, sourceFile):
     return variance
         
         
+# function for extracting Range       
 def Range(number_of_rows, sourceFile):
     
     range_of_row = ['range']
@@ -43,6 +47,22 @@ def Range(number_of_rows, sourceFile):
     return range_of_row
 
 
+# function for extracting Velocity
+def Velocity(number_of_rows, number_of_columns, sourceFile):
+    velocity = ['velocity']
+    velocity = np.asarray(velocity)
+    distance = 0
+    time = number_of_columns / frequency                                              
+    for i in range(1, number_of_rows):         
+        for j in range(number_of_columns-1):
+            next = j + 1
+            distance += euclidean(sourceFile.values[j], sourceFile.values[next])
+        vel = distance/time
+        velocity = np.vstack((velocity, vel))
+    return velocity
+
+
+
 def extractFeatures(filelist, source, destination):
     
     for eachfile in range(len(filelist)):               # len(filelist)
@@ -51,7 +71,7 @@ def extractFeatures(filelist, source, destination):
         file = pandas.read_csv(csvfile, header = None)
         file.values[1:] = file.values[1:].astype(float)                             # convert all strings to floats; ignore header columns
         num_rows = len(file)                                                        # number of rows in the dataset
-        
+        num_columns = len(file.values[0])
         # calculate variance
         
         
