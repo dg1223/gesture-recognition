f946b5b4ad20630153a68c6e22242365553dbace
Shamir Alavi <alavi1223@hotmail.com>
2015-10-07 19:37:35 -0400

    random test script

--- test.py
+++ test.py
@@ -5,28 +5,72 @@ Created on Tue Jun 09 21:54:14 2015
 @author: Shamir
 """
 
-peak_width = []
-width_counter, index = 0, 0
-for noise in range(len(noisy_index)):                                 # check for every pair because each pair of euclidean peaks corresponds to one noisy/corrupted datapoint
-    if index == len(noisy_index) - 1:                                       # break loop if it's the end of row
-        break
-    elif index > 0:
-        index += peak_width[width_counter - 1]
-        if index < len(noisy_index) - 1 and noisy_index[index+1] == noisy_index[index] + 1:
-            peak_width.append(1)
-            if index < len(noisy_index) - 2 and noisy_index[index+2] == noisy_index[index+1] + 1:
-                peak_width[width_counter] = 2
-                if index < len(noisy_index) - 3 and noisy_index[index+3] == noisy_index[index+2] + 1:
-                    peak_width[width_counter] = 3
-        width_counter += 1
-        index += 1
-        if index > len(noisy_index):
-            break
-    else:                                                                   # noise = 0                
-        peak_width.append(1)                                                # minimum peak width = 1
-        if index < len(noisy_index) - 2 and noisy_index[index+2] == noisy_index[index+1] + 1:
-            peak_width[width_counter] = 2
-            if index < len(noisy_index) - 3 and noisy_index[index+3] == noisy_index[index+2] + 1:
-                peak_width[width_counter] = 3
-        width_counter += 1
-        index += 1
+try:
+                for k in range(num_columns):                
+                    #print 'file = ', i, "row = ", j, "col = ", k
+                    output_list.append(firstFile.loc [j, k])
+                    output_list.append(secondFile.loc[j, k])
+                    output_list.append(thirdFile.loc [j, k])
+                    output_list.append(fourthFile.loc[j, k])
+            except KeyError:
+                #error = e.message[-26:-23]
+                try:
+                    num_columns = len(secondFile.values[0])
+                    for k in range(num_columns):                
+                        #print 'file = ', i, "row = ", j, "col = ", k
+                        output_list.append(firstFile.loc [j, k])
+                        output_list.append(secondFile.loc[j, k])
+                        output_list.append(thirdFile.loc [j, k])
+                        output_list.append(fourthFile.loc[j, k])
+                except KeyError:
+                    try:
+                        num_columns = len(thirdFile.values[0])
+                        for k in range(num_columns):                
+                            #print 'file = ', i, "row = ", j, "col = ", k
+                            output_list.append(firstFile.loc [j, k])
+                            output_list.append(secondFile.loc[j, k])
+                            output_list.append(thirdFile.loc [j, k])
+                            output_list.append(fourthFile.loc[j, k])
+                    except KeyError:
+                        num_columns = len(fourthFile.values[0])
+                        for k in range(num_columns):                
+                            #print 'file = ', i, "row = ", j, "col = ", k
+                            output_list.append(firstFile.loc [j, k])
+                            output_list.append(secondFile.loc[j, k])
+                            output_list.append(thirdFile.loc [j, k])
+                            output_list.append(fourthFile.loc[j, k])
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+            except KeyError as e:
+                if num_columns > 1000: 
+                    error = int(e.message[-26:-22])
+                    print 'columns > 1000 ', 'error value = ', error
+                    num_columns = error
+                    for k in range(num_columns):                
+                        #print 'file = ', i, "row = ", j, "col = ", k
+                        output_list.append(firstFile.loc [j, k])
+                        output_list.append(secondFile.loc[j, k])
+                        output_list.append(thirdFile.loc [j, k])
+                        output_list.append(fourthFile.loc[j, k])
+                else:
+                    error = int(e.message[-26:-23])
+                    print 'columns < 1000 ', 'error value = ', error
+                    num_columns = error
+                    for k in range(num_columns):                
+                        #print 'file = ', i, "row = ", j, "col = ", k
+                        output_list.append(firstFile.loc [j, k])
+                        output_list.append(secondFile.loc[j, k])
+                        output_list.append(thirdFile.loc [j, k])
+                        output_list.append(fourthFile.loc[j, k])
