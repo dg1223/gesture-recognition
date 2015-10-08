67871829de0921f0420ce7b4158486edc575d40e
Shamir Alavi <alavi1223@hotmail.com>
2015-10-07 21:16:46 -0400

    random test script

--- test.py
+++ test.py
@@ -74,3 +74,45 @@ try:
                         output_list.append(secondFile.loc[j, k])
                         output_list.append(thirdFile.loc [j, k])
                         output_list.append(fourthFile.loc[j, k])
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+# Left Hand Gestures
+    print 'Calculating LH Features'
+    
+    print 'Calculating variance'
+    variance_l = Variance(source_left)
+    print 'Calculating range'
+    range_l    = Range(source_left)
+    print 'Calculating velocity'
+    velocity_l = Velocity(source_left)
+    print 'Calculating angular velocity'
+    AngVel_l   = AngularVelocity(source_left_Euclid)
+    print 'Calculating covariance'
+    covar_l    = Covariance(source_left)
+    
+    fullFile_l = pandas.concat([variance_l, range_l, velocity_l, AngVel_l, covar_l], axis = 1)
+    fullFile_l.to_csv(destination + left + fileformat, header = False, index = False)
