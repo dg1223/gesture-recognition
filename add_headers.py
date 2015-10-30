4a98a43cc48f642b83e0cd9733e2c4d8e5437d2a
Shamir Alavi <alavi1223@hotmail.com>
2015-10-30 17:10:51 -0400

    Add Headers (bug detected!)
    overwrites 1st row of each file with the headers instead of making a new
    row

--- add_headers.py
+++ add_headers.py
@@ -5,11 +5,15 @@ Created on Tue Oct 06 20:58:30 2015
 @author: Shamir
 """
 
+## BIG BUG!!! : It deletes the 1st row!!! Please swap left and right in 'sortLeftRight' | solution: insert empty header row 1st
+
+
+
 import pandas
 import os
 import numpy as np
 
-sourcePath = 'C:\\Users\\Shamir\\Desktop\\Grad\\Participant Study\\CSV Files\\P3\\'
+sourcePath = 'C:\\Users\\Shamir\\Desktop\\Grad\\Participant Study\\CSV Files\\P11\\'
 fileformat = '.csv'
 backslash = '\\'
 header = np.array(['qr','qx','qy','qz','time'])
@@ -20,13 +24,14 @@ for i in range(len(os.listdir(sourcePath))):                                    
     filelist = os.listdir(sourcePath + gesture)
           
     for k in range(len(os.listdir(sourcePath + gesture))):    
-        file = filelist[k]          # desired csv file in the folder 
-        print 'k = ', k, 'file = ', file
-        csvfile = sourcePath + gesture + backslash + file   # full filepath
+        File = filelist[k]          # desired csv file in the folder 
+        print 'k = ', k, 'file = ', File
+        csvfile = sourcePath + gesture + backslash + File   # full filepath
         readFile = pandas.read_csv(csvfile)
         number_of_columns = len(readFile.values[0])        
         #print number_of_columns
         Header = np.tile(header, number_of_columns/5)
+        #readFile.insert(0, )
         
         # this calculation does not yield 100% correct (bug free) result >> P3 (Uppercut): had a few 'nan' values in the header columns > corrected manually       
         if number_of_columns % 5 == 1:
@@ -45,7 +50,7 @@ for i in range(len(os.listdir(sourcePath))):                                    
             Header = Header.tolist()
             Header.append('qr', 'qx', 'qy', 'qz')
             Header = np.asarray(Header)
-        #readFile.to_csv(sourcePath + gesture + backslash + file[0:-4] + fileformat, header = Header, index = None)
+        readFile.to_csv(sourcePath + gesture + backslash + File[0:-4] + fileformat, header = Header, index = None)
     
     
     
